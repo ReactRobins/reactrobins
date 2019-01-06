0a10d38e57c5e95621645aaa1407a24e0f40f423
niharikak101 <niharikak101@gmail.com>
2019-01-06 22:26:14 +0530

    ui nits

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: 'React Robins',
-    description:
-      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    description: 'Official Website for ReactRobins',
+    author: 'Emily Mears & Niharika Khanna',
     menuLinks: [
       {
         name: 'About',
